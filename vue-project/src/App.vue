4a5fd4f265e1ccb51f6231709b30f9f9f6624816
RmartV <134070727+RmartV@users.noreply.github.com>
2025-01-27 11:30:27 +0800

    added mango

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,19 @@
-<script></script>
- 
 <template>
   <h1>Food</h1>
   <food-item/>
+  <food-item2/>
   <food-item/>
-  <food-item/>
+  <personal-profile/> <br>
 </template>
 
 <script></script>
 
 <style>
   #app > div {
-        border: dashed black 1px;
-        display: inline-block;
-        margin: 10px;
-        padding: 10px;
-        background-color: lightgreen;
-    }
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  } 
 </style>
