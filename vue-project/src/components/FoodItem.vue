dcfb41292e99622696b89dbac628f9ef6dfbf8ab
RmartV <134070727+RmartV@users.noreply.github.com>
2025-01-27 10:59:29 +0800

    food item

--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="apple">
+    <div>
         <h2>{{ name }}</h2>
         <p>{{ message }}</p>
     </div>
@@ -16,11 +16,4 @@ export default {
 };
 </script>
 
-<style> 
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen; 
-      }</style>
+<style></style>
