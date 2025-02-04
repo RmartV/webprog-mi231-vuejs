b514a6368f4551e667831956497d6dd58c06d2ea
RmartV <134070727+RmartV@users.noreply.github.com>
2025-02-04 22:22:15 +0800

    fixed

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -5,37 +5,23 @@
     </ul>
   </template>
   
-  <script></script>
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from '../lib/supabaseClient'
+  
+  const comments = ref([])
+  
+  async function getComments() {
+    const { data } = await supabase.from('comments').select()
+    comments.value = data
+  }
+  
+  onMounted(() => {
+    getComments()
+  })
+  </script>
   
   <style>
-    #app > div {
-      border: dashed black 1px;
-      display: inline-block;
-      margin: 10px;
-      padding: 10px;
-      background-color: lightyellow;
-    }
-  </style>
-
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from '../lib/supabaseClient'
-
-const comments = ref([])
-
-async function getComments() {
-  const { data } = await supabase.from('comments').select()
-  comments.value = data
-}
-
-onMounted(() => {
-  getComments()
-})
-
-</script>
-
-
-<style>
   #app > div {
     border: dashed black 1px;
     display: inline-block;
@@ -43,4 +29,4 @@ onMounted(() => {
     padding: 10px;
     background-color: lightyellow;
   }
-</style>
+  </style>
